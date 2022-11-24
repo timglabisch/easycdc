49ed478cb0a5d8088469469dd172ebe8c727dae6
timglabisch <glabisch@easybill.de>
2022-11-24 16:00:31 +0100

    ....

--- src/sink/benchmark/mod.rs
+++ src/sink/benchmark/mod.rs
@@ -1,6 +1,9 @@
+use std::sync::atomic::{AtomicU64, Ordering};
 use crate::cdc::CdcStream;
 use crate::config::ConfigSinkBenchmark;
 use crate::control_handle::ControlHandleReceiver;
+
+pub static COUNTER : AtomicU64 = AtomicU64::new(0);
 
 pub struct SinkBenchmark {
     config: ConfigSinkBenchmark,
@@ -24,12 +27,16 @@ impl SinkBenchmark {
     pub fn run(mut self) {
         ::tokio::spawn(async move {
             self.run_inner().await.expect("sink benchmark panic");
+            let number_if_items = COUNTER.swap(0, Ordering::Relaxed);
+
+
         });
     }
 
     pub async fn run_inner(&mut self) -> Result<(), ::anyhow::Error> {
         loop {
             let _ = self.cdc_stream.recv().await?;
+            COUNTER.fetch_add(1, Ordering::Relaxed);
         }
     }
 }
